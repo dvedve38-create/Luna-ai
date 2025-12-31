d2a6a107b031304464116622523985692b21a1a3
dvedve38-create <dvedve38@gmail.com>
2025-12-31 16:52:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello, I am Luna 🌙")
+while True:
+    command = input("You: ").lower()
+
+    if "call" in command:
+        print("Luna: Calling contact 📞")
+    elif "bluetooth" in command:
+        print("Luna: Bluetooth connected 🔵")
+    elif "exit" in command:
+        print("Luna: Goodbye 🌙")
+        break
+    else:
+        print("Luna: I did not understand")
+
